14dbece9f8cb51d0dde4f459a37ab28b74f330c2
nityapriya-sahu <sahunityapriya@gmail.com>
2024-09-17 23:49:00 +0530

    basic structure of application

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home_container">
+      <div className="profile_side">1</div>
+      <div className="post_side">2</div>
+      <div className="right_side">3</div>
+    </div>
+  );
+};
+
+export default Home;
